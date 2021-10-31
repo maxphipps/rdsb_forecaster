d5a9a20548ce51fba7143a3ff03ae79a71b5bef1
Max Phipps <maxphipps4@gmail.com>
2021-10-31 14:02:53 +0000

    Improve cmg resolution to daily

--- utils/cmg_utils.py
+++ utils/cmg_utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import pathlib
 
@@ -6,6 +7,36 @@ import seaborn as sns
 sns.set()
 
 path = pathlib.PurePath('data/cmg')
+
+
+def __get_daily_cmg_data(cmg_fn, tag) -> pd.DataFrame:
+    """
+    Loads daily commodities data.
+    This data is available for the past 10 years only.
+    :param cmg_fn: Commodities filename
+    :param tag: Tag for this commodity
+    :return:
+    """
+    df = pd.read_csv(path.joinpath(cmg_fn), parse_dates=['date'], skiprows=15)
+    df = df.rename(columns={'date': 'Date', ' value': tag})
+    df = df.set_index('Date')
+
+    # Cull to daterange of interest
+    df = df[df.index > '2008-01-01']
+
+    #### START OF RESAMPLING
+    # Force pandas to resample beginning at a specific data (note how this is end of fiscal year - important)
+    df.loc[pd.Timestamp('2007-12-29')] = None
+    df = df.sort_index()  # sorting by index
+
+    # Fill & interpolate missing dates (resampling):
+    df = df.resample('D')
+    df = df.interpolate(method='polynomial', order=2)
+    # df = df.ffill()
+
+    # df = df.reset_index()
+
+    return df
 
 def get_cmg() -> pd.DataFrame:
     # Natural gas in dollars per thousand cubic feet
@@ -29,29 +60,41 @@ def get_cmg() -> pd.DataFrame:
                         # 'NGRCUUS': 'Natural Gas Price Residential Sector',
                         }
 
-    from datetime import datetime
     # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     dateparse = lambda x: datetime.strptime(x, '%b %Y')
     df = pd.read_csv(path.joinpath('eia/prices/energy_prices.csv'), parse_dates=['source key'], date_parser=dateparse, skiprows=5)
     df = df.rename(columns={'source key': 'Date', **cols_of_interest})
     df = df.set_index('Date')
+    df = df.sort_index()  # sorting by index
 
-    # Convert index date to datetime type
-    # df.index = pd.to_datetime(df)
+    # Values are the mean of the daily spot prices for the entire month, including the 1st date in the next month
+    # E.g. "Jul 2021" refers to mean of 01/07/2021 to 02/08/2021 spot prices
+    # Since spot prices are readily available, simply shift to mid-month, and interpolate
+    df.index = df.index + timedelta(days=14)
+    df = df.resample('D')
+    df = df.interpolate(method='polynomial', order=2)
+
+    # Override with exact daily values where available
+    df_brent = __get_daily_cmg_data('brent-crude-oil-prices-10-year-daily-chart.csv', 'brent')
+    df_wticrude = __get_daily_cmg_data('wti-crude-oil-prices-10-year-daily-chart.csv', 'wticrude')
+
+    # # Visual data verification
+    # ax = plt.gca()
+    # df['wticrude'].plot(ax=ax)
+    # df_wticrude.plot(ax=ax)
+    # plt.show()
+    #
+    # ax = plt.gca()
+    # df['brent'].plot(ax=ax)
+    # df_brent.plot(ax=ax)
+    # plt.show()
+    # assert False
+
+    df['brent'] = df_brent.dropna().combine_first(df[['brent']])
+    df['wticrude'] = df_wticrude.dropna().combine_first(df[['wticrude']])
 
     # Cull to daterange of interest
     # df = df[df.index > '2008-01-01']
-
-    #### START OF RESAMPLING
-    # Force pandas to resample beginning at a specific data (note how this is end of fiscal year - important)
-    # df.loc[pd.Timestamp('2007-12-29')] = None
-    df = df.sort_index()  # sorting by index
-
-    # Fill & interpolate missing dates (resampling):
-    df = df.resample('D')
-    # df = df.interpolate(method='polynomial', order=2)
-    df = df.ffill()
-    df = df.bfill()
 
     # Slice to columns of interest
     df = df[cols_of_interest.values()]
@@ -68,6 +111,7 @@ def get_cmg() -> pd.DataFrame:
 
     return df
 
+
 def get_oil_demand() -> pd.DataFrame:
     cols_of_interest = {'PAPR_WORLD': 'world production',
                         'PATC_WORLD': 'world consumption',
@@ -79,15 +123,11 @@ def get_oil_demand() -> pd.DataFrame:
                         'PASC_US': 'U.S. Commercial Inventory Crude Oil and Other Liquids',  # million barrels
                         }
 
-    from datetime import datetime
     # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     dateparse = lambda x: datetime.strptime(x, '%b %Y')
     df = pd.read_csv(path.joinpath('eia/supply_demand/production_consumption_and_inventories.csv'), parse_dates=['source key'], date_parser=dateparse, skiprows=4)
     df = df.rename(columns={'source key': 'Date', **cols_of_interest})
     df = df.set_index('Date')
-
-    # Convert index date to datetime type
-    # df.index = pd.to_datetime(df)
 
     # Cull to daterange of interest
     # df = df[df.index > '2008-01-01']
