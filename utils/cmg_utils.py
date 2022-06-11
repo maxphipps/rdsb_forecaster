aee69f4583dee4b30d9fead0d7ed3bac91621957
Max Phipps <maxphipps4@gmail.com>
2022-06-11 15:30:04 +0100

    Clean commodities utilities

--- utils/cmg_utils.py
+++ utils/cmg_utils.py
@@ -1,6 +1,7 @@
 import pathlib
 from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
@@ -9,36 +10,34 @@ sns.set()
 path = pathlib.PurePath('data/cmg')
 
 
-def __get_daily_cmg_data(cmg_fn, tag) -> pd.DataFrame:
-    """
-    Loads daily commodities data.
-    This data is available for the past 10 years only.
-    :param cmg_fn: Commodities filename
-    :param tag: Tag for this commodity
-    :return:
-    """
-    df = pd.read_csv(path.joinpath(cmg_fn), parse_dates=['date'], skiprows=15, sep=';')
-    df = df.rename(columns={'date': 'Date', ' value': tag})
-    df = df.set_index('Date')
+def get_cmg(corr_plot=False) -> pd.DataFrame:
+    def get_daily_cmg_data(cmg_fn, tag) -> pd.DataFrame:
+        """
+        Loads daily commodities data.
+        This data is available for the past 10 years only.
+        :param cmg_fn: Commodities filename
+        :param tag: Tag for this commodity
+        :return:
+        """
+        df = pd.read_csv(path.joinpath(cmg_fn), parse_dates=['date'], skiprows=15, sep=';')
+        df = df.rename(columns={'date': 'Date', ' value': tag})
+        df = df.set_index('Date')
 
-    # Cull to daterange of interest
-    df = df[df.index > '2008-01-01']
+        # Slice to daterange of interest
+        df = df[df.index > '2008-01-01']
 
-    #### START OF RESAMPLING
-    # Force pandas to resample beginning at a specific data (note how this is end of fiscal year - important)
-    df.loc[pd.Timestamp('2007-12-29')] = None
-    df = df.sort_index()  # sorting by index
+        # Force pandas to resample beginning at a specific data
+        # Note: this is end of fiscal year - important
+        df.loc[pd.Timestamp('2007-12-29')] = None
+        df = df.sort_index()  # sorting by index
 
-    # Fill & interpolate missing dates (resampling):
-    df = df.resample('D')
-    df = df.interpolate(method='polynomial', order=2)
-    # df = df.ffill()
+        # Fill & interpolate missing dates:
+        df = df.resample('D')
+        df = df.interpolate(method='polynomial', order=2)
+        # df = df.ffill()
 
-    # df = df.reset_index()
+        return df
 
-    return df
-
-def get_cmg() -> pd.DataFrame:
     # Natural gas in dollars per thousand cubic feet
     # WTI and brent in dollars per barrel
     # gasoline and diesel fuel in cents per gallon
@@ -69,91 +68,47 @@ def get_cmg() -> pd.DataFrame:
 
     # Values are the mean of the daily spot prices for the entire month, including the 1st date in the next month
     # E.g. "Jul 2021" refers to mean of 01/07/2021 to 02/08/2021 spot prices
-    # # Since spot prices are readily available, simply shift to mid-month, and interpolate.
-    # #   Cannot do this, since inconsistent with average
-    # df.index = df.index + timedelta(days=14)
-    # df = df.resample('D')
-    # df = df.interpolate(method='polynomial', order=2)
     # Simply ffill to the next month
     df = df.reindex(df.index.union([df.index[-1] + timedelta(days=30)]))
     df = df.resample('D').ffill()
 
     # Override with exact daily values where available
-    df_brent = __get_daily_cmg_data('macrotrendsdotnet/brent-crude-oil-prices-10-year-daily-chart.csv', 'brent')
-    df_wticrude = __get_daily_cmg_data('macrotrendsdotnet/wti-crude-oil-prices-10-year-daily-chart.csv', 'wticrude')
-
-    # # Visual data verification
-    # ax = plt.gca()
-    # df['wticrude'].plot(ax=ax)
-    # df_wticrude.plot(ax=ax)
-    # plt.show()
-    #
-    # ax = plt.gca()
-    # df['brent'].plot(ax=ax)
-    # df_brent.plot(ax=ax)
-    # plt.show()
-    # assert False
+    df_brent = get_daily_cmg_data('macrotrendsdotnet/brent-crude-oil-prices-10-year-daily-chart.csv', 'brent')
+    df_wticrude = get_daily_cmg_data('macrotrendsdotnet/wti-crude-oil-prices-10-year-daily-chart.csv', 'wticrude')
 
     df['brent'] = df_brent.dropna().combine_first(df[['brent']])
     df['wticrude'] = df_wticrude.dropna().combine_first(df[['wticrude']])
 
-    # Cull to daterange of interest
-    # df = df[df.index > '2008-01-01']
-
     # Slice to columns of interest
     df = df[cols_of_interest.values()]
 
-    # # calculate the correlation matrix
-    # corr = df.corr()
-    # print(corr)
-    # # plot the heatmap
-    # sns.heatmap(corr,
-    #             xticklabels=corr.columns,
-    #             yticklabels=corr.columns)
-    # plt.tight_layout()
-    # plt.show()
+    if corr_plot:
+        corr = df.corr()
+        print(corr)
+        sns.heatmap(corr,
+                    xticklabels=corr.columns,
+                    yticklabels=corr.columns)
+        plt.tight_layout()
+        plt.show()
 
     return df
 
 
 def get_oil_demand() -> pd.DataFrame:
-    # cols_of_interest = {#'PAPR_WORLD': 'world production',
-    #                     #'PATC_WORLD': 'world consumption',
-    #                     #'COPR_OPEC': 'OPEC 13 Crude Oil Production',
-    #                     # 'PATC_OECD_EUROPE': 'Europe Petroleum Consumption',
-    #                     # 'PATC_US': 'U.S. (50 States) Petroleum Consumption',
-    #                     # '': '',
-    #                     # '': '',
-    #                     'T3_STCHANGE_WORLD': 'world inventory net withdrawals',  # million barrels per day
-    #                     'PASC_US': 'U.S. Commercial Inventory Crude Oil and Other Liquids',  # million barrels
-    #                     }
-
-    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
     dateparse = lambda x: datetime.strptime(x, '%b %Y')
     df = pd.read_csv(path.joinpath('eia/supply_demand/production_consumption_and_inventories.csv'),
-                     sep=';', parse_dates=['source key'], date_parser=dateparse, index_col=1, # parse_dates=['source key'], skiprows=4,
+                     sep=';',
+                     parse_dates=['source key'], date_parser=dateparse, index_col=1,
+                     # parse_dates=['source key'], skiprows=4,
                      na_values='--')
-    # df = df.rename(columns={'source key': 'Date', **cols_of_interest})
     df = df.rename(columns={'source key': 'Date'})
     df = df.set_index('Date')
-
-    # Change in inventories
-    # df['']
 
     # drop empty columns
     df = df.dropna(how='all', axis=1)
 
-    # Cull to daterange of interest
-    # df = df[df.index > '2008-01-01']
-
     # Resample to daily
     df = df.reindex(df.index.union([df.index[-1] + timedelta(days=30)]))
     df = df.resample('D').ffill()
-
-    # Slice to columns of interest
-    # df = df[cols_of_interest.values()]
-
-    # Scale to reasonable size
-    # df *= 1E3
 
     return df
